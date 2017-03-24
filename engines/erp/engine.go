963473966256e0e35987ca04647f1013d06816d9
Jeremy Zheng <jitang.zheng@gmail.com>
2017-03-24 15:17:45 -0700

    erp and mall

---
+++ engines/erp/engine.go
@@ -0,0 +1,51 @@
+package erp
+
+import (
+	"github.com/ikeikeikeike/go-sitemap-generator/stm"
+	"github.com/jinzhu/gorm"
+	"github.com/kapmahc/fly/engines/auth"
+	"github.com/kapmahc/fly/web"
+	"github.com/urfave/cli"
+	"golang.org/x/tools/blog/atom"
+	gin "gopkg.in/gin-gonic/gin.v1"
+)
+
+// Engine engine
+type Engine struct {
+	Db   *gorm.DB  `inject:""`
+	I18n *web.I18n `inject:""`
+	Jwt  *auth.Jwt `inject:""`
+}
+
+// RegisterWorker register worker
+func (p *Engine) RegisterWorker() {
+
+}
+
+// Atom rss.atom
+func (p *Engine) Atom(lang string) ([]*atom.Entry, error) {
+	return []*atom.Entry{}, nil
+}
+
+// Sitemap sitemap.xml.gz
+func (p *Engine) Sitemap() ([]stm.URL, error) {
+	return []stm.URL{}, nil
+}
+
+// Dashboard dashboard
+func (p *Engine) Dashboard(c *gin.Context) *web.Dropdown {
+	return nil
+}
+
+// Shell shell commands
+func (p *Engine) Shell() []cli.Command {
+	return []cli.Command{}
+}
+
+// Mount web mount-points
+func (p *Engine) Mount(rt *gin.Engine) {
+}
+
+func init() {
+	web.Register(&Engine{})
+}
--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 	_ "github.com/kapmahc/fly/engines/blog"
 	_ "github.com/kapmahc/fly/engines/erp"
 	_ "github.com/kapmahc/fly/engines/forum"
+	_ "github.com/kapmahc/fly/engines/mall"
 	_ "github.com/kapmahc/fly/engines/ops/mail"
 	_ "github.com/kapmahc/fly/engines/ops/vpn"
 	_ "github.com/kapmahc/fly/engines/reading"
-	_ "github.com/kapmahc/fly/engines/shop"
 	_ "github.com/kapmahc/fly/engines/site"
 	"github.com/kapmahc/fly/web"
 )
